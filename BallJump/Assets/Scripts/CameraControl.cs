ff967173ec5db362be0f3d90e689e2be0ee4adeb
Blake Roles <blakeroles@gmail.com>
2020-05-24 10:29:15 +1000

    working on camera control

---
+++ BallJump/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+
+	public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+    	if (target.position.y > 0)
+    	{
+
+        	Vector3 targetPos = new Vector3(transform.position.x, target.position.y, transform.position.z);
+
+        	transform.position = targetPos;
+    	}
+
+
+    }
+}
