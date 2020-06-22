bb4b7474ac534d5a42621a6517bce0b6382bfc3f
Blake Roles <blakeroles@gmail.com>
2020-06-22 20:57:44 +1000

    v1.0 - Sprint 5 - add adds and update font colors

--- BallJump/Assets/Scripts/AdBanner.cs
+++ BallJump/Assets/Scripts/AdBanner.cs
@@ -1,19 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-// using GoogleMobileAds.Api;
+using GoogleMobileAds.Api;
 
 public class AdBanner : MonoBehaviour
 {
 
-	// private BannerView bannerView;
+	private BannerView bannerView;
 
     // Start is called before the first frame update
     void Start()
     {	
-        // MobileAds.Initialize(initStatus => { });
+        MobileAds.Initialize(initStatus => { });
 
-        // this.RequestBanner();
+        this.RequestBanner();
     }
 
     // Update is called once per frame
@@ -24,19 +24,19 @@ public class AdBanner : MonoBehaviour
 
     private void RequestBanner()
     {
-    	// #if UNITY_ANDROID
-    	// 	string adUnitId = "ca-app-pub-3940256099942544/6300978111";
-    	// #elif UNITY_IPHONE
-    	// 	string adUnitId = "ca-app-pub-3940256099942544/2934735716";
-    	// #else
-    	// 	string adUnitId = "unexpected_platform";
-    	// #endif
+    	#if UNITY_ANDROID
+    		string adUnitId = "ca-app-pub-3940256099942544/6300978111";
+    	#elif UNITY_IPHONE
+    		string adUnitId = "ca-app-pub-3940256099942544/2934735716";
+    	#else
+    		string adUnitId = "unexpected_platform";
+    	#endif
 
-    	// this.bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Top);
+    	this.bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Bottom);
 
-    	// AdRequest request = new AdRequest.Builder().Build();
+    	AdRequest request = new AdRequest.Builder().Build();
 
-    	// this.bannerView.LoadAd(request);
+    	this.bannerView.LoadAd(request);
 
     }
 }
